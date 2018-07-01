dbf81c9a013976aa6a715a2dae6f0223f78a832b
stephcmartin <martin.stephaniechow@gmail.com>
2018-06-30 21:21:05 -0400

    Added redux form

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,26 +1,58 @@
 import React , { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-export default class RegisterForm extends Component {
+class RegisterForm extends Component {
   render () {
     return (
-
       <form>
       <fieldset>
         <label>First Name:</label>
+        <Field
+        name="first_name"
+        type="text"
+        component="input"
+        />
+
       </fieldset>
 
       <fieldset>
         <label>Last Name:</label>
+        <Field
+        name="last_name"
+        type="text"
+        component="input"
+        />
+      </fieldset>
+
+      <fieldset>
+        <label>Email</label>
+        <Field
+        name="email"
+        type="text"
+        component="input"
+        />
       </fieldset>
 
       <fieldset>
         <label>Password:</label>
+        <Field
+        name="password"
+        type="password"
+        component="input"
+        />
       </fieldset>
 
       <fieldset>
         <label>Confirm Password:</label>
+        <Field
+        name="confirm_password"
+        type="password"
+        component="input"
+        />
       </fieldset>
       </form>
     )
   }
 }
+
+export default reduxForm({form: 'signup' })(RegisterForm);
