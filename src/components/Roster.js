32abcd7e9c3c9198f0d67157d93b1ed2ecb1ca17
stephcmartin <martin.stephaniechow@gmail.com>
2018-07-01 19:09:18 -0400

    Added roster component

---
+++ src/components/Roster.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Roster extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Your roster of players:</h2>
+            </div>
+        )
+    }
+}
+
+export default Roster;
