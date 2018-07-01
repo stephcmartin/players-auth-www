9e8929593ed4dbb394668fcce25c841b849c259a
stephcmartin <martin.stephaniechow@gmail.com>
2018-07-01 19:52:48 -0400

    Added login action

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,6 +17,7 @@ export const register = (formProps, callback) => async dispatch => {
   }
 };
 
+// User Sign Out
 export const signout = () => {
   localStorage.removeItem('token');
   return {
@@ -24,3 +25,20 @@ export const signout = () => {
     payload: ''
   }
 }
+
+// User Login
+
+export const login = (formProps, callback) => async dispatch => {
+  // Create user
+  try{
+    const response = await axios.post(
+      'https://players-api.developer.alchemy.codes/api/login', 
+      formProps);
+
+    dispatch({ type: AUTH_USER, payload: response.data.token});
+    localStorage.setItem('token', response.data.token);
+    callback();
+  } catch (e) {
+      dispatch({ type: AUTH_ERROR, payload: 'Invalid login credentials'})
+  }
+};
